d9f77881ebf6ac3af60f6890b7865898d7961605
Hrishikesh Tiwari <tiwarihrishikesh686@gmail.com>
2025-12-27 23:28:20 +0530

    otsu bug resolved

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,8 +2,8 @@ import peda_img
 import numpy as np
 
 def test_gray_conversion():
-    img = peda_img.handle_image("test_images/satellite.png")
-    thrs_img = peda_img.threshold_image(img)
+    img = peda_img.handle_image("test_images/oppieBW.png")
+    thrs_img = peda_img.otsu_thresholding(img, inverse=True)
     peda_img.show_image(thrs_img)
 
 test_gray_conversion()
